85129c2b83726f9558ca9071ee9681ed2802bc61
listless-dude <sd2021667@gmail.com>
2026-02-13 02:23:41 +0530

    checking printing moves

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "move.h"
 #include "bitboard.h"
 #include "utils.h"
+#include "movesgen.h"
 
 void init_board(Board *board);
 void print_board(Board *board);
@@ -12,7 +13,14 @@ int main()
     Board board;
     init_board(&board);
     
+    MoveList list = {0};
+
+    pawn_moves(&board, &list);
     print_board(&board);
 
+    for (int i = 0; i < list.count; i++)
+    {
+        print_move(list.moves[i]);
+    }
     return 0;
 }
