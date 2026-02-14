e455d91f41a8b241a4b15012eb8e5feb28146800
listless-dude <sd2021667@gmail.com>
2026-02-15 02:35:45 +0530

    testing queen moves

--- mainTest.cpp
+++ mainTest.cpp
@@ -16,6 +16,7 @@ int main()
     // remove pawn at a2 to free rook
     pop_bit(&board.bitboards[wP], 8);
     pop_bit(&board.bitboards[wP], 9);
+    pop_bit(&board.bitboards[wP], 10);
     pop_bit(&board.bitboards[wP], 11);
 
     // rebuild occupancies
@@ -41,8 +42,10 @@ int main()
     printf("After rooks: %d\n", list.count);
 
     bishop_moves(&board, &list);
-    printf("After rooks: %d\n", list.count);
+    printf("After bishop: %d\n", list.count);
 
+    queen_moves(&board, &list);
+    printf("After queen: %d\n", list.count);
     print_board(&board);
 
     for (int i = 0; i < list.count; i++)
